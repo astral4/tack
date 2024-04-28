089fd9b48e3a30a7211c734f6026b89f923922ff
astral4 <88992929+astral4@users.noreply.github.com>
2024-04-28 00:09:34 -0400

    parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    todo!()
+use anyhow::{anyhow, Context, Result};
+use std::{env::args_os, path::PathBuf};
+
+fn main() -> Result<()> {
+    let mut args = args_os();
+
+    let input_path: PathBuf = args
+        .next()
+        .ok_or_else(|| anyhow!("Input image path was not provided"))?
+        .into();
+
+    let output_path: PathBuf = args
+        .next()
+        .ok_or_else(|| anyhow!("Output thumbnail path was not provided"))?
+        .into();
+
+    let thumbnail_size: usize = args
+        .next()
+        .ok_or_else(|| anyhow!("Thumbnail size was not provided"))?
+        .to_str()
+        .ok_or_else(|| anyhow!("Thumbnail size was not valid UTF-8"))?
+        .parse()
+        .context("Failed to parse the provided thumbnail size as a number")?;
+
+    Ok(())
 }
